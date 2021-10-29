3aa42ec0514471f86853f347cdad1655695daaf2
joebrooks <lcw3176@naver.com>
2021-10-29 13:18:27 +0900

    메인 내용 변경

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,46 +1,77 @@
-#include "arduinoFFT.h"
+#define LENGTH 256
+#define sample_freq 8919
+byte rawData[LENGTH];
+int count = 0;
 
-arduinoFFT FFT = arduinoFFT();
-const uint16_t samples = 64; //This value MUST ALWAYS be a power of 2
-const double samplingFrequency = 5000;
-double vReal[samples];
-double vImag[samples];
+unsigned int signalMax = 0;
+unsigned int signalMin = 1024;
 
-void setup()
-{
+void setup(){
   Serial.begin(115200);
   analogReference(EXTERNAL);
 }
+void loop(){
+  
+  if (count < LENGTH) {
+    count++;
+    int sample = analogRead(A0);
+    rawData[count] = sample;
 
-void loop()
-{
-  double frequency;
-  double volume; 
-  
-  for (uint8_t i = 0; i < samples; i++)
-  {
-    vReal[i] = analogRead(A0);
-    delayMicroseconds(100);
-    vImag[i] = 0;
-  }
-  
-  FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);  /* Weigh data */
-  FFT.Compute(vReal, vImag, samples, FFT_FORWARD); /* Compute FFT */
-  FFT.ComplexToMagnitude(vReal, vImag, samples); /* Compute magnitudes */
+    if(sample > signalMax){
+      signalMax = sample;
+    } else if(sample < signalMin){
+      signalMin = sample;
+    }
+    
+  } else {
+    long sum = 0;
+    long sum_old = 0;
+    int freq_per = 0;
+    byte pd_state = 0;
+    int thresh = 0;
+    int period = 0;
+    int len = sizeof(rawData);
+    
+    for(int i = 0; i < len; i++)
+    {
+      sum_old = sum;
+      sum = 0;
+      
+      for(int k = 0; k < len-i; k++) {
+        sum += (rawData[k]-128)*(rawData[k+i]-128) / 256;
+      }
+      
+      // Peak Detect State Machine
+      if (pd_state == 2 && (sum-sum_old) <=0) 
+      {
+        period = i;
+        pd_state = 3;
+      }
+      if (pd_state == 1 && (sum > thresh) && (sum-sum_old) > 0) {
+        pd_state = 2;
+      }
+      if (!i) {
+        thresh = sum * 0.5;
+        pd_state = 1;
+      }
+    }
+
+    double volts = ((signalMax - signalMin) * 3.3) / 1024;
+    // Frequency identified in Hz
  
-  FFT.MajorPeak(vReal, samples, samplingFrequency, &frequency, &volume);
-  
-  String freStr = String(frequency, DEC);
-  String volStr = String(volume, DEC);
-  
-  for(int i = 0; i < freStr.length(); i++){
-    Serial.write(freStr[i]);
+    if (thresh >100 && period != 0 && volts >= 0.15) {
+      freq_per = sample_freq / period;
+      String data = String(freq_per);
+ 
+      for(int j = 0; j < data.length(); j++){
+        Serial.write(data[j]);
+      }
+     
+      Serial.write("\n");
+    }
+
+    count = 0;
+    signalMax = 0;
+    signalMin = 1024;
   }
-  
-  Serial.write(",");
-  
-  for(int i = 0; i < volStr.length(); i++){
-    Serial.write(volStr[i]);
-  }
-  
 }
